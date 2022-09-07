7ef876442de935e58a859c5f0f98a284f5c9b355
Chris Johnston <cjohnston@connected.io>
2022-09-07 16:29:03 -0400

    initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+
+require_relative File.join(File.dirname(__FILE__), '..', 'lib', 'kata.rb')
+
+pattern = File.join(File.dirname(__FILE__), 'support', '**', '*.rb')
+Dir[pattern].each { |filepath| require_relative filepath }
